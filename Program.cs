ad4e184516455ac2b7598fd4e2d5ffcdb7dfdbe0
Sai-adi <masterful.adi@gmail.com>
2025-05-17 09:23:42 +0530

    day1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Your Name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter Your DOB Year: ");
+        string ageInput = Console.ReadLine();
+        int age = int.Parse(ageInput);
+
+        int currentYear = DateTime.Now.Year;
+
+        int newage = currentYear - age;
+
+        Console.WriteLine($"Your Name is {name} and age is {newage}");
+        if (newage > 18)
+        {
+            Console.WriteLine("You are adult");
+        }
+        else
+        {
+            Console.WriteLine("You are minor");
+        }
+    }
+}
