e773dd047f98f97f7142526f96dd45ee3ef5fa5d
Sai-adi <masterful.adi@gmail.com>
2025-05-17 15:18:51 +0530

    if else statements

--- Login.cs
+++ Login.cs
@@ -1,23 +1,24 @@
 using System;
 
-class Login
+class checkScore()
 {
     static void Main()
     {
+        Console.Write("Enter Your Score");
+        string score = Console.ReadLine();
+        int newScore = int.Parse(score);
 
+        if (newScore < 100 && newScore > 90)
+            Console.WriteLine("A Grade");
 
-        Console.WriteLine("Enter username: ");
-        string username = Console.ReadLine();
+        else if (newScore < 90 && newScore > 80)
+            Console.WriteLine("B Grade");
 
-        Console.WriteLine("Enter Password: ");
-        string password = Console.ReadLine();
-
-        if (username == "asdfghjkl" && password == "asdfghjkl")
-        {
-            Console.WriteLine("Correct credentials");
-        }
-        else {
-            Console.WriteLine("Wrong Credentials");
-        }
+        else if (newScore < 80 && newScore > 70)
+            Console.WriteLine("C Grade");
+        else if (newScore < 70 && newScore > 60)
+            Console.WriteLine("D Grade");
+        else if (newScore < 60)
+            Console.WriteLine("F Grade");
     }
 }
